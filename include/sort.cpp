3b48374caea4966503208e6361c758a116648d4d
GavrilovN <coinflipper97@yandex.ru>
2016-12-04 22:48:20 +0300

    Add files via upload

---
+++ include/sort.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+struct s_i
+{
+	string s;
+	ifstream *f;
+	s_i(const string& s_,ifstream* f_) : s(s_), f(f_){}
+	bool operator < (const s_i& s_i_) const
+	{
+		return (s > s_i_.s);
+	}
+};
+
+void sort_by_TAHK(const string input_name, const string output_name, const unsigned long int mem_size)
+{
+	ifstream fin(input_name);
+	if (!fin.is_open()) throw("file_not_open");
+	ofstream fout(output_name);
+	size_t k = 0;
+	while (!fin.eof())
+	{
+		ofstream fout_(to_string(k + 1) + ".txt");
+		vector<string> v; string s;
+		for (unsigned long int size = 0; (size + sizeof(string)) <= mem_size; size += sizeof(string))
+		{
+			getline(fin, s);
+			v.push_back(s);
+		}
+		sort(v.begin(), v.end());
+		for (auto i : v)
+		{
+			if (i != "") fout_ << i<<endl;
+		}
+		++k;
+		fout_.close();
+	}
+	fin.close();
+	priority_queue<s_i> pq;
+	for (size_t i = 0; i < k; ++i)
+	{
+		ifstream* f_ = new ifstream(to_string(i + 1) + ".txt");
+		string str;
+		getline(*f_, str);
+		s_i si(str, f_);
+		pq.push(si);
+	}
+	while (!pq.empty())
+	{
+		s_i si = pq.top();
+		pq.pop();
+		if (si.s != "")
+		{
+			fout << si.s;
+			/*if (!pq.empty())*/ fout << endl;
+		}
+		if (!(*si.f).eof())
+		{
+			getline(*si.f, si.s);
+			pq.push(si);
+		}
+		else
+		{
+			(*(si.f)).close();
+		}
+	}
+	for (size_t i = 0; i < k; ++i)
+	{
+		remove((to_string(i + 1) + ".txt").c_str());
+	}
+	fout.close();
+}
+
+/*void main(){
+string s_in, s_out;
+unsigned long int memory_size;
+cout << "input_name: "; cin >> s_in;
+cout << "output_name: "; cin >> s_out;
+cout << "free memory(bytes): "; cin >> memory_size;
+sort_by_TAHK(s_in, s_out, memory_size);
+system("pause");
+}*/
---
+++ tests/init.cpp
@@ -0,0 +1,26 @@
+#include <sort_by_TAHK.cpp>
+#include <catch.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+SCENARIO("zzz", "[zzz]")
+{
+  sort_by_TAHK("in.txt", "out.txt", 120);
+	string *a = new string[21], *b = new string[21];
+	bool x = false;
+	ifstream f1("out.txt"), f2("test.txt");
+	for (int i = 0; i < 21; ++i)
+	{
+		getline(f1, a[i]);
+		getline(f2, b[i]);
+	}
+	size_t x_ = 0;
+	for (int i = 0; i < 21; ++i)
+	{
+		if (a[i] == b[i]) ++x_;
+	}
+	if (x_ == 21) x = true;
+  REQUIRE(x);
+} 
