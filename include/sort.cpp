f472407de4aa6d95b23f3e576ade2c7b7465ffa2
GavrilovN <coinflipper97@yandex.ru>
2016-12-04 23:03:16 +0300

    Update sort.cpp

--- include/sort.cpp
+++ include/sort.cpp
@@ -4,38 +4,80 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <chrono>
 using namespace std;
 
-struct s_i
+struct stroka
 {
-	string s;
-	ifstream *f;
-	s_i(const string& s_,ifstream* f_) : s(s_), f(f_){}
-	bool operator < (const s_i& s_i_) const
+	string name;
+	string surname;
+	short year;
+	size_t size() const
 	{
-		return (s > s_i_.s);
+		size_t sz = sizeof(string);
+		return (sz + name.size() + sz + surname.size() + sizeof(short));
 	}
 };
 
-void sort_it(const string input_name, const string output_name, const unsigned long int mem_size)
+bool operator < (const stroka& s1, const stroka& s2)
 {
-	ifstream fin(input_name);
+	return (s1.name < s2.name);
+}
+
+bool operator >(const stroka& s1, const stroka& s2)
+{
+	return (s1.name > s2.name);
+}
+
+ostream & operator<<(ostream & output, stroka const & str)
+{
+	output << str.surname << " " << str.name << " " << str.year;
+	return output;
+}
+
+istream & operator>>(istream & input, stroka & str)
+{
+	input >> str.surname >> str.name >> str.year;
+	return input;
+}
+
+bool operator != (const stroka& s, const string& str)
+{
+	return (s.surname != str);
+}
+
+struct s_i
+{
+	stroka s;
+	ifstream *f;
+	s_i(const stroka& s_, ifstream* f_) : s(s_), f(f_){}
+};
+
+bool operator < (const s_i& s_i1, const s_i& s_i2)
+{
+	return (s_i1.s > s_i2.s);
+}
+
+void sort_by_TAHK(const string input_name, const string output_name, const short mem_size)
+{
+	ifstream fin(input_name, ios::binary);
 	if (!fin.is_open()) throw("file_not_open");
-	ofstream fout(output_name);
-	size_t k = 0;
+	ofstream fout(output_name, ios::binary);
+	short k = 0;
+	const size_t ms = mem_size * 1024 * 1024 * 0.63;
 	while (!fin.eof())
 	{
-		ofstream fout_(to_string(k + 1) + ".txt");
-		vector<string> v; string s;
-		for (unsigned long int size = 0; (size + sizeof(string)) <= mem_size; size += sizeof(string))
+		vector<stroka> v; stroka s;
+		ofstream fout_(to_string(k + 1), ios::binary);
+		for (unsigned long int size = 0; (size + 50) < ms;)
 		{
-			getline(fin, s);
-			v.push_back(s);
+			if (!fin.eof() && (fin >> s) && (s != ""))  v.push_back(s);
+			size += s.size();
 		}
 		sort(v.begin(), v.end());
 		for (auto i : v)
 		{
-			if (i != "") fout_ << i<<endl;
+			if (i != "") fout_ << i << endl;
 		}
 		++k;
 		fout_.close();
@@ -44,9 +86,9 @@ void sort_it(const string input_name, const string output_name, const unsigned l
 	priority_queue<s_i> pq;
 	for (size_t i = 0; i < k; ++i)
 	{
-		ifstream* f_ = new ifstream(to_string(i + 1) + ".txt");
-		string str;
-		getline(*f_, str);
+		ifstream* f_ = new ifstream(to_string(i + 1), ios::binary);
+		stroka str;
+		*f_ >> str;
 		s_i si(str, f_);
 		pq.push(si);
 	}
@@ -54,14 +96,9 @@ void sort_it(const string input_name, const string output_name, const unsigned l
 	{
 		s_i si = pq.top();
 		pq.pop();
-		if (si.s != "")
+		if (si.s != "") fout << si.s << endl;
+		if (!(*si.f).eof() && (*si.f >> si.s))
 		{
-			fout << si.s;
-			fout << endl;
-		}
-		if (!(*si.f).eof())
-		{
-			getline(*si.f, si.s);
 			pq.push(si);
 		}
 		else
@@ -71,7 +108,7 @@ void sort_it(const string input_name, const string output_name, const unsigned l
 	}
 	for (size_t i = 0; i < k; ++i)
 	{
-		remove((to_string(i + 1) + ".txt").c_str());
+		remove((to_string(i + 1)).c_str());
 	}
 	fout.close();
 }
